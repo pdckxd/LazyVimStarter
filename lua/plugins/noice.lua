e4ea7d3d502788dc4b981c46a97b7894bf730478
Alex to <pdckxd@gmail.com>
2024-02-27 13:29:55 +0800

    add config for noice.nvim plugin to show cmdline_popup win in middle-center of the window

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        -- show cmdline popup in middle-center of the window
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
+  },
+}

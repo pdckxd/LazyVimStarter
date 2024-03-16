5b128682767a7b077a2218252cc1df68e01ff701
Alex Chen <pdckxd@gmail.com>
2024-03-16 15:07:02 +0800

    update: use conf property to determine plugin enablement

--- lua/plugins/snip_run.lua
+++ lua/plugins/snip_run.lua
@@ -1,15 +1,23 @@
-local uname = vim.loop.os_uname()
-local sysname = uname["sysname"]
---NOTE: Not support Windows
-if string.find(string.lower(sysname), "windows") ~= nil then
-  vim.print("WARN: sniprun plugin does not support Windows!")
-  return {}
-end
+-- local uname = vim.loop.os_uname()
+-- local sysname = uname["sysname"]
+-- --NOTE: Not support Windows
+-- if string.find(string.lower(sysname), "windows") ~= nil then
+--   vim.print("WARN: sniprun plugin does not support Windows!")
+--   return {}
+-- end
 
 return {
   "michaelb/sniprun",
   branch = "master",
   build = "sh install.sh",
+  cond = function()
+    local uname = vim.loop.os_uname()
+    local sysname = uname["sysname"]
+    if string.find(string.lower(sysname), "windows") ~= nil then
+      return false
+    end
+    return true
+  end,
   -- do 'sh install.sh 1' if you want to force compile locally
   -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
   opts = {

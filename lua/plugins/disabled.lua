c43b608655dcc5c324a42e3c825973b0a89b17be
Alex Chen <pdckxd@gmail.com>
2024-07-23 10:56:25 +0800

    update: lazyvim/starter

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "folke/flash.nvim", enabled = false },
+  { "normen/vim-pio", enabled = false },
   -- { "stevearc/dressing.nvim", enabled = false },
   -- NOTE: lsp status progress bar won't show if disable noice.nvim
   -- { "folke/noice.nvim", enabled = false },

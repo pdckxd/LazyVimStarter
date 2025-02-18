9d23a9fc61b8dac0f5bf46a725ef72b36bbe806b
Alex Chen <pdckxd@gmail.com>
2025-02-18 17:35:29 +0800

    (update) lua/cofnig/options to disable text width setting for gitcommit ft, disable gitcommitBlank syntax group (update) lazyvim.json to enable mini-surround plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable text width setting for git commit filetype
+vim.cmd([[
+  au FileType gitcommit setlocal tw=0
+  highlight link gitcommitBlankxxx NONE
+]])
+
+-- disable gitcommitBlank syntax group
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.cmd("highlight link gitcommitBlank NONE")
+  end,
+})
